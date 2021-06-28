7cce80677721e2cf62339fc01b9365522cabf308
Lázaro <lazarodu@gmail.com>
2021-06-28 08:43:41 -0300

    Aula do dia 28/06/2021

--- controller/Form.php
+++ controller/Form.php
@@ -8,6 +8,10 @@ class Form
   public function controller()
   {
     $form = new Template("view/form.html");
+    $form->set("id", "");
+    $form->set("paciente", "");
+    $form->set("diag", "");
+    $form->set("idade", "");
     $retorno["msg"] = $form->saida();
     return $retorno;
   }
@@ -21,11 +25,20 @@ class Form
         $diag = $conexao->quote($_POST["diag"]);
         $idade = $conexao->quote($_POST["idade"]);
         $crud = new Crud();
-        $retorno = $crud->insert(
-          "consultorio",
-          "paciente,diag,idade",
-          "{$paciente},{$diag},{$idade}"
-        );
+        if (empty($_POST["id"])) {
+          $retorno = $crud->insert(
+            "consultorio",
+            "paciente,diag,idade",
+            "{$paciente},{$diag},{$idade}"
+          );
+        } else {
+          $id = $conexao->quote($_POST["id"]);
+          $retorno = $crud->update(
+            "consultorio",
+            "paciente={$paciente}, diag={$diag}, idade={$idade}",
+            "id={$id}"
+          );
+        }
       } catch (Exception $e) {
         $retorno["msg"] = "Ocorreu um erro! " . $e->getMessage();
         $retorno["erro"] = TRUE;
--- controller/Tabela.php
+++ controller/Tabela.php
@@ -45,6 +45,36 @@ class Tabela
     return $retorno;
   }
 
+  public function editar()
+  {
+    if (isset($_GET["id"])) {
+      try {
+        $conexao = Transaction::get();
+        $id = $conexao->quote($_GET["id"]);
+        $crud = new Crud();
+        $retorno = $crud->select(
+          "consultorio",
+          "*",
+          "id={$id}"
+        );
+        if (!$retorno["erro"]) {
+          $form = new Template("view/form.html");
+          foreach ($retorno["msg"][0] as $key => $value) {
+            $form->set($key, $value);
+          }
+          $retorno["msg"] = $form->saida();
+        }
+      } catch (Exception $e) {
+        $retorno["msg"] = "Ocorreu um erro! " . $e->getMessage();
+        $retorno["erro"] = TRUE;
+      }
+    } else {
+      $retorno["msg"] = "Faltando parâmetro! ";
+      $retorno["erro"] = TRUE;
+    }
+    return $retorno;
+  }
+
   public function __destruct()
   {
     Transaction::close();
